1960131302ec80ce826381c162f3d6208bd2e55d
ducky <asqrybot@gmail.com>
2026-09-10 14:24:09 -0400

    feat: add some constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,14 @@
 const config = {
-  brand_color: 0x79aa92,
+  brand_color: 0x2BA572,
+  brand_color_canary: 0xB933AF,
+  client_id: "1545764581417291816",
+  canary_client_id: "1545933466196254761",
+  web_url: "https://dev.ducky.wiki",
+  auth_url: "https://staging-auth.toolrinth.com",
   images: {
     icon: "https://files.ducky.wiki/share/public_assets/external/ModrinthBot/ModrinthLogo.png"
-  }
+  },
+  official_accounts: ['modrinth', 'automod', 'modrintharchives']
 }
 
 export default config;
