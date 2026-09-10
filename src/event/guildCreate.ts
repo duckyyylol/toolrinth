5fa5663f00a5526bddfe90768f5516c294946602
ducky <asqrybot@gmail.com>
2026-09-10 14:24:28 -0400

    db: migrate to postgres

--- src/event/guildCreate.ts
+++ src/event/guildCreate.ts
@@ -1,11 +1,14 @@
 import { Guild } from "discord.js"
 import { DB } from "../db/DB"
+import { ContextTypes } from "../types";
 
 export default {
   enabled: true,
   run: async (guild: Guild) => {
-    if (!DB.Guilds.getGuild(guild.id)) {
-      DB.Guilds.createGuild({ id: guild.id });
+    if (guild) {
+      if (!DB.Contexts.getContext(guild.id)) {
+        DB.Contexts.createContext({ id: guild.id, type: ContextTypes.GUILD });
+      }
     }
   }
 }
